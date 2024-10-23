5e26d5d67b40014b5c28d51fba77db1a4aff577f
Carline Senat <carlinesenat@gmail.com>
2024-10-22 18:08:24 -0700

    Followed along with practices
    Updated README to follow instructions to project. Made injections to my pug files.
    Reinitialized express to be the latest updated version and to be able to test on port 3000

--- app.js
+++ app.js
@@ -2,4 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+];
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+    console.log(`This is the about page.. or should be`)
+});
+
+app.get('/project/:id', (req, res) => {
+    res.send("Hi, welcome to my site!");
+    console.log(`Render of a customized version of the pug project template...`)
+});
+
+app.get('/noroute', (req, res) => {
+    res.send("Hi, welcome to my site!");
+});
+
+app.listen(3000, () => {
+    console.log(`Running on localhost:3000.`);
+});
